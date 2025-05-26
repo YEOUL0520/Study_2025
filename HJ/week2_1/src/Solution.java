bf28cf062f2fe5c876e9a310f0860f1cf5254f10
HYOJEONG RYU <flowtothemoon355@gmail.com>
2025-05-26 12:49:52 +0900

    [2주차] 올바른 괄호 / 하

---
+++ HJ/week2_1/src/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 문자 하나씩 떼고
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                count++;
+            }
+            else {
+                count--;
+                if (count < 0)
+                    return false; // ( 이거 없는데 ) 얘를 빼줌
+            }
+        }
+        return count == 0;  // 0이면 true, 아니면 false 반환 (boolean solution)
+    }
+}
